fa0509c2969bf65f23f1b57c5c570aa47fbd2dca
Adimote <Adimote@users.noreply.github.com>
2021-11-10 23:46:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ async def on_message(message: discord.Message) -> None:
             role_name = SPECIAL_ROLE
         else:
             # Add them to the 'verified' role.
-            # This doesn't happen in special cases because we expect a second step before verifying them.
+            # This doesn't happen in special cases because we expect a second step (outside of this bot) before verifying them.
             role: discord.Role = discord.utils.get(message.guild.roles, name=VERIFIED_ROLE)
             await message.author.add_roles(role, reason="A correct password was entered.")
             
